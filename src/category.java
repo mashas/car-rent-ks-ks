0f3f7af4129c99f346fa6e4decad50715607c839
Ann Sakhno <anya_sakhno@mail.ru>
2014-04-22 01:03:48 +0300

    I've renamed it. Other must looks like it

---
+++ src/category.java
@@ -0,0 +1,15 @@
+import java.util.Vector;
+
+public class category {
+
+  public String nameClass;
+
+    public vector  myvehicle;
+
+  public void addclass() {
+  }
+
+  public void removeclass() {
+  }
+
+}
